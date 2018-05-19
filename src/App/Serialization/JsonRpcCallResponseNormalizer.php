1220754950108cd61857f5b1e59781f65c9cf275
Yoanm <yoanm@users.noreply.github.com>
2018-05-19 15:17:49 +0200

    JsonRpcCallResponseNormalizer

---
+++ src/App/Serialization/JsonRpcCallResponseNormalizer.php
@@ -0,0 +1,52 @@
+<?php
+namespace Yoanm\JsonRpcServer\App\Serialization;
+
+use Yoanm\JsonRpcServer\Domain\Model\JsonRpcCallResponse;
+
+/**
+ * Class JsonRpcCallResponseNormalizer
+ */
+class JsonRpcCallResponseNormalizer
+{
+    /** @var JsonRpcResponseNormalizer */
+    private $responseNormalizer;
+
+    /**
+     * @param JsonRpcResponseNormalizer $responseNormalizer
+     */
+    public function __construct(JsonRpcResponseNormalizer $responseNormalizer)
+    {
+        $this->responseNormalizer = $responseNormalizer;
+    }
+
+    /**
+     * @param JsonRpcCallResponse $jsonRpcCallResponse
+     *
+     * @return array|null
+     */
+    public function normalize(JsonRpcCallResponse $jsonRpcCallResponse)
+    {
+        $resultList = [];
+        foreach ($jsonRpcCallResponse->getResponseList() as $response) {
+            // Notifications must not have a response, even if they are on error
+            if (!$response->isNotification()) {
+                $resultList[] = $this->responseNormalizer->normalize($response);
+            }
+        }
+
+        // if no result, it means It was either :
+        // - a batch call with only notifications
+        // - a notification request
+        // => return null response in all cases
+        if (0 === count($resultList)) {
+            return null;
+        }
+
+        // In case it's not a batch, return the first (lonely) result
+        if (!$jsonRpcCallResponse->isBatch()) {
+            return array_shift($resultList);
+        }
+
+        return $resultList;
+    }
+}
