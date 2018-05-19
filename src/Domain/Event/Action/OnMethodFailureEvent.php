575d74aff0d7521e513a1726a73e3492e3c60ed8
Yo <yoanm@users.noreply.github.com>
2018-05-19 14:46:10 +0200

    Action events (#43)

---
+++ src/Domain/Event/Action/OnMethodFailureEvent.php
@@ -0,0 +1,53 @@
+<?php
+namespace Yoanm\JsonRpcServer\Domain\Event\Action;
+
+use Yoanm\JsonRpcServer\Domain\JsonRpcMethodInterface;
+use Yoanm\JsonRpcServer\Domain\Model\JsonRpcRequest;
+
+/**
+ * Class OnMethodFailureEvent
+ *
+ * Dispatched only in case JSON-RPC method thrown an exception.
+ */
+class OnMethodFailureEvent extends AbstractOnMethodEvent
+{
+    const EVENT_NAME = 'json_rpc_server_skd.on_method_failure';
+
+    /** @var \Exception */
+    private $exception;
+
+    /**
+     * @param \Exception             $exception
+     * @param JsonRpcMethodInterface $method
+     * @param JsonRpcRequest         $jsonRpcRequest
+     */
+    public function __construct(
+        \Exception $exception,
+        JsonRpcMethodInterface $method,
+        JsonRpcRequest $jsonRpcRequest
+    ) {
+        $this->exception = $exception;
+
+        parent::__construct($method, $jsonRpcRequest);
+    }
+
+    /**
+     * @return \Exception
+     */
+    public function getException() : \Exception
+    {
+        return $this->exception;
+    }
+
+    /**
+     * @param \Exception $exception
+     *
+     * @return OnMethodFailureEvent
+     */
+    public function setException(\Exception $exception) : OnMethodFailureEvent
+    {
+        $this->exception = $exception;
+
+        return $this;
+    }
+}
