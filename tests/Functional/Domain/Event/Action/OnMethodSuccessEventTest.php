89948ba5099c40cc1595d3f3268506b79c1ac361
Yoanm <yoanm@users.noreply.github.com>
2018-05-19 13:18:49 +0200

    ActionEvents TT/FT

---
+++ tests/Functional/Domain/Event/Action/OnMethodSuccessEventTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Tests\Functional\Domain\Event\Action;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Yoanm\JsonRpcServer\Domain\Event\Action\OnMethodSuccessEvent;
+use Yoanm\JsonRpcServer\Domain\JsonRpcMethodInterface;
+use Yoanm\JsonRpcServer\Domain\Model\JsonRpcRequest;
+
+/**
+ * @covers \Yoanm\JsonRpcServer\Domain\Event\Action\OnMethodSuccessEvent
+ * @covers \Yoanm\JsonRpcServer\Domain\Event\Action\AbstractOnMethodEvent
+ *
+ * @group Events
+ * @group ActionEvents
+ */
+class OnMethodSuccessEventTest extends TestCase
+{
+    public function testShouldManageAResultFromAMethodAndARequest()
+    {
+        $result = 'my-result';
+        /** @var JsonRpcMethodInterface|ObjectProphecy $method */
+        $method = $this->prophesize(JsonRpcMethodInterface::class);
+        /** @var JsonRpcRequest|ObjectProphecy $request */
+        $request = $this->prophesize(JsonRpcRequest::class);
+
+        $event = new OnMethodSuccessEvent($result, $method->reveal(), $request->reveal());
+
+        $this->assertSame($result, $event->getResult());
+        $this->assertSame($method->reveal(), $event->getMethod());
+        $this->assertSame($request->reveal(), $event->getJsonRpcRequest());
+    }
+
+    public function testShouldAllowResultChange()
+    {
+        $resultA = 'my-result-a';
+        $resultB = 'my-result-b';
+        /** @var JsonRpcMethodInterface|ObjectProphecy $method */
+        $method = $this->prophesize(JsonRpcMethodInterface::class);
+        /** @var JsonRpcRequest|ObjectProphecy $request */
+        $request = $this->prophesize(JsonRpcRequest::class);
+
+        $event = new OnMethodSuccessEvent($resultA, $method->reveal(), $request->reveal());
+
+        $this->assertSame($resultA, $event->getResult());
+
+        $event->setResult($resultB);
+        $this->assertSame($resultB, $event->getResult());
+    }
+}
