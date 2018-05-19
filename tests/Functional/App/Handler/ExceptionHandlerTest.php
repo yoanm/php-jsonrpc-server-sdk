2092149a0fd80fbb9c24711d336fe34cd8fcb8f6
Yoanm <yoanm@users.noreply.github.com>
2018-05-19 10:45:41 +0200

    ExceptionHandler TT/FT

--- tests/Functional/App/Handler/ExceptionHandlerTest.php
+++ tests/Functional/App/Handler/ExceptionHandlerTest.php
@@ -6,84 +6,63 @@ use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
 use Yoanm\JsonRpcServer\App\Creator\ResponseCreator;
 use Yoanm\JsonRpcServer\App\Handler\ExceptionHandler;
-use Yoanm\JsonRpcServer\App\Handler\JsonRpcRequestHandler;
-use Yoanm\JsonRpcServer\App\Serialization\JsonRpcCallSerializer;
-use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnBatchSubRequestProcessedEvent;
-use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnBatchSubRequestProcessingEvent;
-use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnRequestReceivedEvent;
-use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnResponseSendingEvent;
-use Yoanm\JsonRpcServer\Domain\Event\Action\OnExceptionEvent;
-use Yoanm\JsonRpcServer\Domain\JsonRpcServerDispatcherInterface;
-use Yoanm\JsonRpcServer\Domain\Model\JsonRpcCall;
 use Yoanm\JsonRpcServer\Domain\Model\JsonRpcRequest;
 use Yoanm\JsonRpcServer\Domain\Model\JsonRpcResponse;
-use Yoanm\JsonRpcServer\Infra\Endpoint\JsonRpcEndpoint;
 
 /**
  * @covers \Yoanm\JsonRpcServer\App\Handler\ExceptionHandler
+ *
+ * @group ExceptionHandler
  */
-class ExceptionHandlerWithDispatcherTest extends TestCase
+class ExceptionHandlerTest extends TestCase
 {
     /** @var ExceptionHandler */
     private $exceptionHandler;
     /** @var ResponseCreator */
     private $responseCreator;
-    /** @var JsonRpcServerDispatcherInterface|ObjectProphecy */
-    private $jsonRpcServerDispatcher;
 
     public function setUp()
     {
         $this->responseCreator = $this->prophesize(ResponseCreator::class);
-        $this->jsonRpcServerDispatcher = $this->prophesize(JsonRpcServerDispatcherInterface::class);
 
         $this->exceptionHandler = new ExceptionHandler(
             $this->responseCreator->reveal()
         );
-
-        $this->exceptionHandler->setJsonRpcServerDispatcher($this->jsonRpcServerDispatcher->reveal());
     }
 
-    public function testShouldDispatchEventBeforeCreatingResponseAndUseExceptionFromEvent()
+    public function testShouldRelyOnResponseCreator()
     {
         /** @var ObjectProphecy|\Exception $fakeException */
         $fakeException = $this->prophesize(\Exception::class);
+        /** @var JsonRpcResponse|ObjectProphecy $response */
+        $response = $this->prophesize(JsonRpcResponse::class);
 
-        $this->responseCreator->createErrorResponse(Argument::cetera())
-            ->willReturn($this->prophesize(JsonRpcResponse::class)->reveal())
+        $this->responseCreator->createErrorResponse($fakeException->reveal(), null)
+            ->willReturn($response->reveal())
             ->shouldBeCalled();
 
-        $this->exceptionHandler->getJsonRpcResponseFromException($fakeException->reveal());
-
-        $this->jsonRpcServerDispatcher->dispatchJsonRpcEvent(
-            'json_rpc_server_skd.on_exception',
-            Argument::allOf(
-                Argument::type(OnExceptionEvent::class),
-                Argument::which('getFromJsonRpcRequest', null),
-                Argument::which('getException', $fakeException->reveal())
-            )
-        )->shouldHaveBeenCalled();
+        $this->assertSame(
+            $response->reveal(),
+            $this->exceptionHandler->getJsonRpcResponseFromException($fakeException->reveal())
+        );
     }
 
-    public function testShouldDispatchEventWithRequestBeforeCreatingResponseAndUseExceptionFromEvent()
+    public function testShouldRelyOnResponseCreatorAndPassRequest()
     {
-        /** @var ObjectProphecy|\Exception $fakeException */
-        $fakeException = $this->prophesize(\Exception::class);
-        /** @var ObjectProphecy|JsonRpcRequest $fakeRequest */
+        /** @var JsonRpcRequest|ObjectProphecy $fakeRequest */
         $fakeRequest = $this->prophesize(JsonRpcRequest::class);
+        /** @var ObjectProphecy|\Exception $fakeException */
+        $fakeException = $this->prophesize(\Exception::class);
+        /** @var JsonRpcResponse|ObjectProphecy $response */
+        $response = $this->prophesize(JsonRpcResponse::class);
 
-        $this->responseCreator->createErrorResponse(Argument::cetera())
-            ->willReturn($this->prophesize(JsonRpcResponse::class)->reveal())
+        $this->responseCreator->createErrorResponse($fakeException->reveal(), $fakeRequest->reveal())
+            ->willReturn($response->reveal())
             ->shouldBeCalled();
 
-        $this->exceptionHandler->getJsonRpcResponseFromException($fakeException->reveal(), $fakeRequest->reveal());
-
-        $this->jsonRpcServerDispatcher->dispatchJsonRpcEvent(
-            'json_rpc_server_skd.on_exception',
-            Argument::allOf(
-                Argument::type(OnExceptionEvent::class),
-                Argument::which('getFromJsonRpcRequest', $fakeRequest->reveal()),
-                Argument::which('getException', $fakeException->reveal())
-            )
-        )->shouldHaveBeenCalled();
+        $this->assertSame(
+            $response->reveal(),
+            $this->exceptionHandler->getJsonRpcResponseFromException($fakeException->reveal(), $fakeRequest->reveal())
+        );
     }
 }
---
+++ tests/Functional/App/Handler/ExceptionHandlerWithDispatcherTest.php
@@ -0,0 +1,91 @@
+<?php
+namespace Tests\Functional\Infra\Endpoint;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+use Yoanm\JsonRpcServer\App\Creator\ResponseCreator;
+use Yoanm\JsonRpcServer\App\Handler\ExceptionHandler;
+use Yoanm\JsonRpcServer\App\Handler\JsonRpcRequestHandler;
+use Yoanm\JsonRpcServer\App\Serialization\JsonRpcCallSerializer;
+use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnBatchSubRequestProcessedEvent;
+use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnBatchSubRequestProcessingEvent;
+use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnRequestReceivedEvent;
+use Yoanm\JsonRpcServer\Domain\Event\Acknowledge\OnResponseSendingEvent;
+use Yoanm\JsonRpcServer\Domain\Event\Action\OnExceptionEvent;
+use Yoanm\JsonRpcServer\Domain\JsonRpcServerDispatcherInterface;
+use Yoanm\JsonRpcServer\Domain\Model\JsonRpcCall;
+use Yoanm\JsonRpcServer\Domain\Model\JsonRpcRequest;
+use Yoanm\JsonRpcServer\Domain\Model\JsonRpcResponse;
+use Yoanm\JsonRpcServer\Infra\Endpoint\JsonRpcEndpoint;
+
+/**
+ * @covers \Yoanm\JsonRpcServer\App\Handler\ExceptionHandler
+ *
+ * @group ExceptionHandler
+ */
+class ExceptionHandlerWithDispatcherTest extends TestCase
+{
+    /** @var ExceptionHandler */
+    private $exceptionHandler;
+    /** @var ResponseCreator */
+    private $responseCreator;
+    /** @var JsonRpcServerDispatcherInterface|ObjectProphecy */
+    private $jsonRpcServerDispatcher;
+
+    public function setUp()
+    {
+        $this->responseCreator = $this->prophesize(ResponseCreator::class);
+        $this->jsonRpcServerDispatcher = $this->prophesize(JsonRpcServerDispatcherInterface::class);
+
+        $this->exceptionHandler = new ExceptionHandler(
+            $this->responseCreator->reveal()
+        );
+
+        $this->exceptionHandler->setJsonRpcServerDispatcher($this->jsonRpcServerDispatcher->reveal());
+    }
+
+    public function testShouldDispatchEventBeforeCreatingResponseAndUseExceptionFromEvent()
+    {
+        /** @var ObjectProphecy|\Exception $fakeException */
+        $fakeException = $this->prophesize(\Exception::class);
+
+        $this->responseCreator->createErrorResponse(Argument::cetera())
+            ->willReturn($this->prophesize(JsonRpcResponse::class)->reveal())
+            ->shouldBeCalled();
+
+        $this->exceptionHandler->getJsonRpcResponseFromException($fakeException->reveal());
+
+        $this->jsonRpcServerDispatcher->dispatchJsonRpcEvent(
+            'json_rpc_server_skd.on_exception',
+            Argument::allOf(
+                Argument::type(OnExceptionEvent::class),
+                Argument::which('getFromJsonRpcRequest', null),
+                Argument::which('getException', $fakeException->reveal())
+            )
+        )->shouldHaveBeenCalled();
+    }
+
+    public function testShouldDispatchEventWithRequestBeforeCreatingResponseAndUseExceptionFromEvent()
+    {
+        /** @var ObjectProphecy|\Exception $fakeException */
+        $fakeException = $this->prophesize(\Exception::class);
+        /** @var ObjectProphecy|JsonRpcRequest $fakeRequest */
+        $fakeRequest = $this->prophesize(JsonRpcRequest::class);
+
+        $this->responseCreator->createErrorResponse(Argument::cetera())
+            ->willReturn($this->prophesize(JsonRpcResponse::class)->reveal())
+            ->shouldBeCalled();
+
+        $this->exceptionHandler->getJsonRpcResponseFromException($fakeException->reveal(), $fakeRequest->reveal());
+
+        $this->jsonRpcServerDispatcher->dispatchJsonRpcEvent(
+            'json_rpc_server_skd.on_exception',
+            Argument::allOf(
+                Argument::type(OnExceptionEvent::class),
+                Argument::which('getFromJsonRpcRequest', $fakeRequest->reveal()),
+                Argument::which('getException', $fakeException->reveal())
+            )
+        )->shouldHaveBeenCalled();
+    }
+}
