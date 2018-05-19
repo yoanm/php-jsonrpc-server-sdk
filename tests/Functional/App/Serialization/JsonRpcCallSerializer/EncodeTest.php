5afe41df1571711d351df45f6c7fdca5da18c32a
Yo <yoanm@users.noreply.github.com>
2018-05-19 15:31:34 +0200

    JsonRpcCallSerializer (#51)

---
+++ tests/Functional/App/Serialization/JsonRpcCallSerializer/EncodeTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Tests\Functional\App\Serialization\JsonRpcCallSerializer;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Yoanm\JsonRpcServer\App\Serialization\JsonRpcCallDenormalizer;
+use Yoanm\JsonRpcServer\App\Serialization\JsonRpcCallResponseNormalizer;
+use Yoanm\JsonRpcServer\App\Serialization\JsonRpcCallSerializer;
+
+/**
+ * @covers \Yoanm\JsonRpcServer\App\Serialization\JsonRpcCallSerializer
+ *
+ * @group JsonRpcCallSerializer
+ * @group Serialization
+ */
+class EncodeTest extends TestCase
+{
+    /** @var JsonRpcCallSerializer */
+    private $jsonRpcCallSerializer;
+    /** @var JsonRpcCallDenormalizer|ObjectProphecy */
+    private $callDenormalizer;
+    /** @var JsonRpcCallResponseNormalizer|ObjectProphecy */
+    private $callResponseNormalizer;
+
+    protected function setUp()
+    {
+        $this->callDenormalizer = $this->prophesize(JsonRpcCallDenormalizer::class);
+        $this->callResponseNormalizer = $this->prophesize(JsonRpcCallResponseNormalizer::class);
+        $this->jsonRpcCallSerializer = new JsonRpcCallSerializer(
+            $this->callDenormalizer->reveal(),
+            $this->callResponseNormalizer->reveal()
+        );
+    }
+
+    public function testShouldJsonEncodeContent()
+    {
+        $decoded = [
+            'a' => 'b',
+            'c' => [
+                'd' => 'e',
+                'f' => [
+                    'g',
+                    'h',
+                    2,
+                    true,
+                    null
+                ]
+            ],
+            'i' => 4,
+            'j' => false,
+            'k' => null
+        ];
+
+        $encoded = $this->jsonRpcCallSerializer->encode($decoded);
+
+        $this->assertSame(
+            $encoded,
+            json_encode($decoded)
+        );
+    }
+}
