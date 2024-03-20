0fab93eedb7824690bf68b35321b6f8602c3a236
Yoanm <4410697+yoanm@users.noreply.github.com>
2024-03-20 08:52:29 +0100

    Clean

--- .github/actions/reports-group/node-sdk/index.js
+++ .github/actions/reports-group/node-sdk/index.js
@@ -4,3 +4,7 @@ export * as path from './src/path-helper'
 export * as find from './src/find'
 export * as load from './src/load'
 export * as outputs from './src/outputs'
+
+// @TODO try yarn workspace again (keep node-sdk as is at first !)
+// @TODO If yarn workspace works, remove node-sdk symlink and replace with a requirement with `workspace:^`
+// @TODO move to TS
