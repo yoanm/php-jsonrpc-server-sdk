d230d23dadaeb7e7a0a000dd30ea8f2de434188f
Yoanm <4410697+yoanm@users.noreply.github.com>
2024-03-22 15:26:22 +0100

    Improve

--- .github/actions/reports-group/attach-check-run-to-triggering-workflow-action/src/cleanup.js
+++ .github/actions/reports-group/attach-check-run-to-triggering-workflow-action/src/cleanup.js
@@ -4,24 +4,26 @@ const core = require('@actions/core');
 const {GITHUB_REPOSITORY} = process.env;
 
 async function run() {
-    if (core.getState('check-run-is-concluded').length) {
+    if (core.getState('check-run-already-concluded').length > 0) {
         core.info('Check run already concluded, skipping check run update');
     }
-
-    const [repoOwner, repoName] = GITHUB_REPOSITORY.split('/');
-    /** INPUTS **/
-    const jobStatus = core.getInput('job-status', {required: true});
     const checkRunId = core.getState('check-run-id');
     if (checkRunId.length === 0) {
         throw new Error('Unable to retrieve check run id !');
     }
+
+    /** INPUTS **/
+    const jobStatus = core.getInput('job-status', {required: true});
     const githubToken = core.getInput('github-token', {required: true});
 
     const requestParams = await core.group(
         'Build API params',
         async () => {
+            const [repoOwner, repoName] = GITHUB_REPOSITORY.split('/');
+
             return {
                 conclusion: jobStatus,
+                // Url path parameters
                 owner: repoOwner,
                 repo: repoName,
                 check_run_id: checkRunId
@@ -30,17 +32,13 @@ async function run() {
     );
     core.debug('API params=' + JSON.stringify(requestParams));
 
-    const apiResponse = await core.group('Retrieve ', async () => {
+    const apiResponse = await core.group('Conclude check-run ', async () => {
         const octokit = github.getOctokit(githubToken);
 
         // @TODO Move back to `octokit.rest.checks.update()`
-        const res = await octokit.request('PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}', requestParams);
-
-        core.info('TMP DEBUG0 ' + JSON.stringify(res));
-
-        return res;
+        return octokit.request('PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}', requestParams);
     });
-    core.info('TMP DEBUG' + JSON.stringify(apiResponse));
+    core.debug('API call to ' +apiResponse.url + ' => HTTP ' + apiResponse.status);
 }
 
 run();
